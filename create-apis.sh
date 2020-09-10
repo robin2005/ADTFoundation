74868af158b672bf49159fbd85777fd0991874ac
robin2005 <57048685@qq.com>
2020-09-10 14:48:53 +0800

    0.0.1

--- create-apis.sh
+++ create-apis.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 file="ADTFoundation"
 # 文档生成
-jazzy --podspec ${file}.podspec --output ../../pages/Github.io/${file}/
+jazzy --podspec ${file}.podspec --output ../robin2005.github.io/${file}/
